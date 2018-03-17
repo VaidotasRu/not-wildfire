29ede2e286a55f1bd75e6401a01815b75c7fe472
VaidotasRu <36315352+VaidotasRu@users.noreply.github.com>
2018-03-17 19:54:54 +0200

    fixed_functions

--- popup.js
+++ popup.js
@@ -4,19 +4,27 @@
 
 
 function startRecording(){
-  console.log("hi");
+  document.body.style.backgroundColor='red';
   //var findBody = document.querySelector('body');
   //findBody.addEventListener('click', convertToJSON);
 }
 
 function stopRecording(){
-
+  document.body.style.backgroundColor='green';
 }
+
+
 
 function play(){
+  document.body.style.backgroundColor='yellow';
 }
+  
+  
+   
+  document.addEventListener('DOMContentLoaded', () =>{
+  document.getElementById('record').addEventListener('click', startRecording);
+  document.getElementById("stop").addEventListener("click", stopRecording);
+  document.getElementById("play").addEventListener("click", play);
+});
 
-document.getElementById("record").addEventListener("click", startRecording());
-document.getElementById("stop").addEventListener("click", stopRecording());
-document.getElementById("play").addEventListener("click", play());
 

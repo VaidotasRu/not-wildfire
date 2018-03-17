f7d047f7ba485a22b34441e20dabc6cdadb71bb3
VaidotasRu <36315352+VaidotasRu@users.noreply.github.com>
2018-03-17 16:04:24 +0200

    jquery

---
+++ functions/record.js
@@ -0,0 +1,20 @@
+// find body element
+var findBody = document.querySelector('body');
+
+// define handler function
+function findContent(event) {
+
+  console.log (event.target.innerHTML);
+}
+
+
+
+// attach event handler
+findBody.addEventListener('click', findContent);
+
+function findPOS(elem)
+{ 
+   var pos;
+
+   return pos;
+}
